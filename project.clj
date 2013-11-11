c0f112223a6a760278e09824f7e7ad5366f73cce
Greg Look <greg@greg-look.net>
2013-11-10 17:36:26 -0800

    Write up README and set up project definition.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject puget "0.1.0-SNAPSHOT"
+  :description "Colorizing canonical Clojure printer for EDN values."
+  :url "https://github.com/greglook/puget"
+  :license {:name "Public Domain"
+            :url "http://unlicense.org/"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/data.codec "0.1.0"]
+                 [fipp "0.4.1"]])
