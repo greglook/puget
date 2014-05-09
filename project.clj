dfad89ca72d6bff602034afcf79424e0b8cb716e
Greg Look <greg@greg-look.net>
2014-05-09 10:45:28 -0700

    Bump snapshot version and use clojure 1.6.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject mvxcvi/puget "0.4.0-SNAPSHOT"
+(defproject mvxcvi/puget "0.5.0-SNAPSHOT"
   :description "Colorizing canonical Clojure printer for EDN values."
   :url "https://github.com/greglook/puget"
   :license {:name "Public Domain"
             :url "http://unlicense.org/"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.codec "0.1.0"]
                  [fipp "0.4.1"]])
