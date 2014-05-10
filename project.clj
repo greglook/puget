f54e26bfb1955ecde637296ec60c97f343a7d961
Greg Look <greg@greg-look.net>
2014-05-10 15:33:46 -0700

    Add cloverage plugin to :test profile.

--- project.clj
+++ project.clj
@@ -3,6 +3,13 @@
   :url "https://github.com/greglook/puget"
   :license {:name "Public Domain"
             :url "http://unlicense.org/"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/data.codec "0.1.0"]
-                 [fipp "0.4.1"]])
+
+  :dependencies
+  [[org.clojure/clojure "1.6.0"]
+   [org.clojure/data.codec "0.1.0"]
+   [fipp "0.4.1"]]
+
+  :profiles
+  {:test
+   {:plugins
+    [[lein-cloverage "1.0.2"]]}})
--- test/coveralls.sh
+++ test/coveralls.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 COVERALLS_URL='https://coveralls.io/api/v1/jobs'
-CLOVERAGE_VERSION='1.0.4-SNAPSHOT' lein2 cloverage --coveralls
+CLOVERAGE_VERSION='1.0.4' lein2 cloverage --coveralls
 curl -F 'json_file=@target/coverage/coveralls.json' "$COVERALLS_URL"
