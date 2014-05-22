91db3f2432041c3fd7b548038117969526bbe5ae
Greg Look <greg@greg-look.net>
2014-05-22 01:16:55 -0700

    Improve test coverage.

--- test/puget/data_test.clj
+++ test/puget/data_test.clj
@@ -5,23 +5,36 @@
     [puget.data :as data]))
 
 
-(defrecord TestRecord [x y])
-(data/extend-tagged-map TestRecord 'test/record)
-(deftest tagged-value-extension
-  (let [rec (TestRecord. :foo :bar)]
-    (is (= 'test/record (data/edn-tag rec)))
-    (is (= {:x :foo, :y :bar} (data/edn-value rec)))))
+(defn test-tagged-value
+  [data t v & [reader]]
+  (is (= t (data/edn-tag data)))
+  (is (= v (data/edn-value data)))
+  (let [s (data/edn-str data)
+        data' (edn/read-string (if reader {:readers {t reader}} {}) s)]
+    (is (= s (pr-str data)))
+    (is (= data data'))))
 
 
-(deftest generic-tagged-value
-  (let [data (data/tagged-value 'foo :bar)
-        string (data/edn-str data)]
-    (is (= 'foo (data/edn-tag data)))
-    (is (= :bar (data/edn-value data)))
-    (is (= data (edn/read-string {:default data/tagged-value} string)))))
+(deftest inst-tagged-values
+  (test-tagged-value
+    (java.util.Date. 1383271402749)
+    'inst "2013-11-01T02:03:22.749-00:00"))
 
 
-(deftest byte-arrays
+(deftest uuid-tagged-values
+  (test-tagged-value
+    (java.util.UUID/fromString "96d91316-53b9-4800-81c1-97ae9f4b86b0")
+    'uuid "96d91316-53b9-4800-81c1-97ae9f4b86b0"))
+
+
+(deftest uri-tagged-values
+  (test-tagged-value
+    (java.net.URI. "http://en.wikipedia.org/wiki/Uniform_resource_identifier")
+    'uri "http://en.wikipedia.org/wiki/Uniform_resource_identifier"
+    data/read-uri))
+
+
+(deftest bin-tagged-values
   (let [byte-arr (.getBytes "foobarbaz")
         string (data/edn-str byte-arr)
         read-arr (edn/read-string {:readers {'bin data/read-bin}} string)]
@@ -31,22 +44,18 @@
     (is (= (seq byte-arr) (seq read-arr)))))
 
 
-(defn test-tagged-value
-  [data t v]
-  (is (= t (data/edn-tag data)))
-  (is (= v (data/edn-value data)))
-  (let [s (data/edn-str data)]
-    (is (= s (pr-str data)))))
+(deftest generic-tagged-value
+  (let [data (data/tagged-value 'foo :bar)
+        string (data/edn-str data)]
+    (is (= 'foo (data/edn-tag data)))
+    (is (= :bar (data/edn-value data)))
+    (is (= "#foo :bar" (pr-str data)))
+    (is (= data (edn/read-string {:default data/tagged-value} string)))))
 
 
-(deftest built-in-printing
-  (testing "TaggedValue"
-    (test-tagged-value
-      (java.util.Date. 1383271402749)
-      'inst "2013-11-01T02:03:22.749-00:00")
-    (test-tagged-value
-      (java.util.UUID/fromString "96d91316-53b9-4800-81c1-97ae9f4b86b0")
-     'uuid "96d91316-53b9-4800-81c1-97ae9f4b86b0")
-    (test-tagged-value
-     (java.net.URI. "http://en.wikipedia.org/wiki/Uniform_resource_identifier")
-     'uri "http://en.wikipedia.org/wiki/Uniform_resource_identifier")))
+(defrecord TestRecord [x y])
+(data/extend-tagged-map TestRecord 'test/record)
+(deftest tagged-value-extension
+  (let [rec (TestRecord. :foo :bar)]
+    (is (= 'test/record (data/edn-tag rec)))
+    (is (= {:x :foo, :y :bar} (data/edn-value rec)))))
