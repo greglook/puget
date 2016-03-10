225c85852d20c0aa06a44e914f0b44cf93eda53c
Greg Look <greg@greg-look.net>
2016-03-09 16:09:07 -0800

    Upgrade arrangement to 1.1.0

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
 
   :dependencies
   [[fipp "0.6.4"]
-   [mvxcvi/arrangement "1.0.0"]
+   [mvxcvi/arrangement "1.1.0"]
    [org.clojure/clojure "1.7.0"]]
 
   :codox {:metadata {:doc/format :markdown}
