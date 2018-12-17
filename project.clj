4560dcaa10641a3542c65ad50cc78f8a62814655
Greg Look <greg@mvxcvi.com>
2018-12-17 10:05:45 -0800

    Bump SNAPSHOT version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mvxcvi/puget "1.1.0-SNAPSHOT"
+(defproject mvxcvi/puget "1.1.1-SNAPSHOT"
   :description "Colorizing canonical Clojure printer for EDN values."
   :url "https://github.com/greglook/puget"
   :license {:name "Public Domain"
