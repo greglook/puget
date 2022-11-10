899fc7e45463c099743ec67b55084b0fc2fdd38a
Greg Look <greg@mvxcvi.com>
2022-11-09 18:21:19 -0800

    Add compatibility tests for Clojure 1.7 - 1.10.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject mvxcvi/puget "1.3.3"
+(defproject mvxcvi/puget "1.3.4-SNAPSHOT"
   :description "Colorizing canonical Clojure printer for EDN values."
   :url "https://github.com/greglook/puget"
   :license {:name "Public Domain"
             :url "http://unlicense.org/"}
 
   :deploy-branches ["main"]
-  :pedantic? :abort
+  :pedantic? :warn
 
   :plugins
   [[lein-cloverage "1.2.2"]]
@@ -13,4 +13,17 @@
   :dependencies
   [[org.clojure/clojure "1.11.1"]
    [mvxcvi/arrangement "2.1.0"]
-   [fipp "0.6.26"]])
+   [fipp "0.6.26"]]
+
+  :profiles
+  {:clj-1.10
+   {:dependencies [[org.clojure/clojure "1.10.3"]]}
+
+   :clj-1.9
+   {:dependencies [[org.clojure/clojure "1.9.0"]]}
+
+   :clj-1.8
+   {:dependencies [[org.clojure/clojure "1.8.0"]]}
+
+   :clj-1.7
+   {:dependencies [[org.clojure/clojure "1.7.0"]]}})
